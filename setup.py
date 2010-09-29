48087e7368033c5c0633e5106573318d3c00e896
Marc Brinkmann <git@marcbrinkmann.de>
2010-09-29 15:41:11 +0200

    setup.py creates admin user.

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
 # coding=utf8
 
+import sys
+
 from dope import db
 import model
 import acl
+
+def read_yesno():
+	while True:
+		l = sys.stdin.readline()
+		if l.lower().startswith('y'): return True
+		if l.lower().startswith('n'): return False
+		sys.stdout.write('Please enter "yes" or "no": ')
+
+
+def read_openid():
+	while True:
+		sys.stdout.write('Enter the OpenID for the admin account: ')
+		openid = sys.stdin.readline().strip()
+		sys.stdout.write('Using OpenID "%s". Is that correct? ' % openid)
+		if read_yesno(): return openid
+
+openid = read_openid()
 
 db.drop_all()
 db.create_all()
@@ -37,5 +56,7 @@ admins.permit('download_file')
 admins.permit('delete_file')
 admins.permit('create_tickets')
 
-# create base ACL
+# create admin account
+u = model.create_new_user(openid)
+u.groups.append(admins)
 db.session.commit()
