2928ea19ebc2a4edb7c4898c709c04208308a437
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-18 11:16:28 +0100

    dope.cfg.example turned to defaults module.

--- app.py
+++ app.py
@@ -4,8 +4,11 @@
 from flask import Flask
 from flaskext.sqlalchemy import SQLAlchemy
 
+import defaults
+
 CONFIGFILE = 'dope.cfg'
 
 app = Flask('dope')
+app.config.from_object(defaults)
 app.config.from_pyfile(CONFIGFILE)
 db = SQLAlchemy(app)
