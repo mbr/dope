4d2c882415be41f13f4fad8fa1bc1074aa3eb428
Marc Brinkmann <git@marcbrinkmann.de>
2011-04-04 12:07:42 +0200

    Added dependencies in setup.py, corrected version.

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@
 from distutils.core import setup
 
 setup(name = 'Dope',
-      version = '1.0',
+      version = '0.1',
       description = 'File uploading made easy (WSGI app).',
       author = 'Marc Brinkmann',
       url = 'https://github.com/mbr/dope',
-      packages = ['dope', 'acl']
+      packages = ['dope', 'acl'],
+      # requirements for acl are just sqlalchemy
+      install_requires = ['sqlalchemy>=0.6.6', 'flask', 'Flask-SQLAlchemy', 'Flask-WTF'],
      )
