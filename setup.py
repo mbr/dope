15494f48d532ab2e69f2f74e2673071b75f381ff
Marc Brinkmann <git@marcbrinkmann.de>
2011-04-04 11:50:04 +0200

    Added first setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from distutils.core import setup
+
+setup(name = 'Dope',
+      version = '1.0',
+      description = 'File uploading made easy (WSGI app).',
+      author = 'Marc Brinkmann',
+      url = 'https://github.com/mbr/dope',
+      packages = ['dope', 'acl']
+     )
